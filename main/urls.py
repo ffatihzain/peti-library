13778457deec4e8f5c0920efcc4238d5078c1caf
ffatihzain <fatihzain10@gmail.com>
2023-11-22 01:16:20 +0700

    create flutter ++

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import create_product_flutter, show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
 from main.views import increase_amount, decrease_amount, delete_item, edit_item
 from main.views import get_product_json, add_item_ajax, delete_item_ajax, edit_item_ajax
@@ -23,4 +23,5 @@ urlpatterns = [
     path('create-item-ajax/', add_item_ajax, name='add_item_ajax'),
     path('delete-item-ajax/<int:item_id>/', delete_item_ajax, name='delete_item_ajax'),
     path('edit-item-ajax/<int:item_id>/', edit_item_ajax, name='edit_item_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
