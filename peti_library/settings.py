0a7396dcc4de82a0af3cafad414a616897433d59
ffatihzain <fatihzain10@gmail.com>
2023-11-22 11:09:04 +0700

    Redeploy Django #5

--- peti_library/settings.py
+++ peti_library/settings.py
@@ -145,3 +145,4 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://muhammad-fatih22-tugas.pbp.cs.ui.ac.id"]
